db7b5b317292aa558bd945983b7d2feb79426732
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2023-03-20 16:33:49 -0300

    styles: Application structure - Colors & font

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -16,9 +16,9 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  ${props => {
-    return css`
-      background-color: ${buttonVariants[props.variant]}
-    `
-  }}
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+ background-color: ${props => props.theme["green-500"]};
+ color: ${props=> props.theme.white};
 `;
