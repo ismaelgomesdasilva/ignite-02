4e2d6fc86c78cee8b9984e80743477c8e6c8d119
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2023-03-20 15:41:57 -0300

    feat: Application structure - Configuring themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { Button } from './components/Button';
 
-function App() {
-
+export default function App() {
   return (
-    <h1>hello</h1>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
   )
 }
-
-export default App
