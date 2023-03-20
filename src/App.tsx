78b91b04b52c79236ee5ddaff7d0a46347ef8645
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2023-03-20 16:25:39 -0300

    styles: Application Structure - Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Button } from './components/Button';
-
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+
+      
+      <GlobalStyle/>
+    </ThemeProvider>
   )
 }
