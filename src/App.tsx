c612eaca76a30fa6860d50dabaa8fb98e83839e2
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2023-07-04 10:05:04 -0300

    Pages and Routes - Route Layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router></Router>
+        <Router />
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
