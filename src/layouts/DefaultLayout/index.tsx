b8ae3f25f5f935a26fa2a2bf3e57ca72c866b45c
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2023-07-04 10:29:43 -0300

    Pages and Routes - Component: Header

---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components/Header'
+import { Outlet } from 'react-router-dom'
+import * as S from './styles'
+export function DefaultLayout() {
+  return (
+    <S.LayoutContainer>
+      <Header />
+      <Outlet />
+    </S.LayoutContainer>
+  )
+}
